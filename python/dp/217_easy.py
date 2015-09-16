798e92780bbdfec088e8241ec6fff259d55eef88
Austin Jackson <austinjackson892@gmail.com>
2015-09-16 13:06:05 +0000

    fixed issue

--- python/dp/217_easy.py
+++ python/dp/217_easy.py
@@ -1,7 +1,10 @@
-d,l,f=input(),input(),''
-for _ in range(d):f+=raw_input()
-f=map(int,f.split())
-for _ in range(l):f[f.index(min(f))]+=1
+d, l, f = input(), input(), ''
+for _ in range(d):
+    f += raw_input()
+f = map(int, f.split())
+for _ in range(l):
+    f[f.index(min(f))] += 1
 for i in range(len(f)):
-    if i%d==0:print'\n',
+    if i % d == 0:
+        print '\n',
     print f[i],
