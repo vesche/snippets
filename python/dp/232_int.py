e0942fb79e2ba5d636b78d682f73e31580b3313c
Austin Jackson <austinjackson892@gmail.com>
2015-09-17 16:59:44 +0000

    fixed list abuse

--- python/dp/232_int.py
+++ python/dp/232_int.py
@@ -1,12 +1,14 @@
 import itertools
 
-data, combos, distances = [], [], []
-
+data = []
 for _ in range(input()):
     data.append(tuple(map(float, raw_input()[1:-1].split(', '))))
 
+control = float('inf')
 for a, b in itertools.combinations(data, 2):
-    combos.append(str(a)+' '+str(b))
-    distances.append(((a[0]-b[0])**2+(a[1]-b[1])**2)**(.5))
+    distance = ((a[0]-b[0])**2+(a[1]-b[1])**2)**(.5)
+    if distance < control:
+        control = distance
+        combo = str(a) + ' ' + str(b)
 
-print combos[distances.index(min(distances))]
+print combo
