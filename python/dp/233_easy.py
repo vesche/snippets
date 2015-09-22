e5f1e838ed4cf2eb3faea6a2f27148d2f6ae28b4
Austin Jackson <austinjackson892@gmail.com>
2015-09-22 14:05:35 +0000

    yuck brb the sequel

--- python/dp/233_easy.py
+++ python/dp/233_easy.py
@@ -1,6 +1,7 @@
 brick, door, edge, roof_left, roof_right, tip, window = '-', '|', '+', '/', \
 '\\', 'A', 'o'
 
+house_space = 's'
 space = door_space = void = ' '
 
 blueprint = []
@@ -21,13 +22,27 @@ def main():
         for _ in range(max_width):
             li.append(void)
     
-    # drop edges and bricks
+    # throw down some brick yo
     for line in range(len(blueprint)):
         for char in range(len(blueprint[line])):
+            if blueprint[line][char] == '*':
+                house[line][funky_width(char):funky_width(char+1)] = [edge, \
+                brick, brick, brick, edge]
+
+    # scrap inners
+    for line in range(len(house)):
+        for char in range(len(house[line])):
             try:
-                if blueprint[line][char]=='*' and blueprint[line-1][char]!='*':
-                    house[line][funky_width(char)] = '+'
-                    house[line][funky_width(char+1)] = '+'
+                # strip bricks
+                if (house[line][char] == brick) and (line != 0) and \
+                (house[line-1][char] in [brick, house_space]):
+                    house[line][char] = house_space
+                # strip edges
+                if (house[line][char] == edge) and (line != 0) and \
+                (house[line][char-1] == house[line][char+1]):
+                    #raw_input(house[line][char-1])
+                    #raw_input(house[line][char+1])
+                    house[line][char] = house[line][char+1]
             except:
                 continue
     
