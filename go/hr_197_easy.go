eed5c85beab746476e57262cc67ce28e8373691e
Austin Jackson <austinjackson892@gmail.com>
2015-10-16 14:29:54 +0000

    fixed whitespace

--- go/hr_197_easy.go
+++ go/hr_197_easy.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+    "fmt"
+    "strconv"
 )
 
 func main() {
-	isbn := "0747532699"
-	if validator(isbn) == true {
-		fmt.Printf("ISBN %v is valid.", isbn)
-	} else {
-		fmt.Printf("ISBN %v is not valid.", isbn)
-	}
+    isbn := "0747532699"
+    if validator(isbn) == true {
+        fmt.Printf("ISBN %v is valid.", isbn)
+    } else {
+        fmt.Printf("ISBN %v is not valid.", isbn)
+    }
 }
 
 func validator(isbn string) bool {
-	sum := 0
+    sum := 0
 
-	for i := 0; i < len(isbn); i++ {
-		numb, _ := strconv.Atoi(string(isbn[i]))
-		sum += (numb * (10 - i))
-	}
+    for i := 0; i < len(isbn); i++ {
+        numb, _ := strconv.Atoi(string(isbn[i]))
+        sum += (numb * (10 - i))
+    }
 
-	return sum%11 == 0
+    return sum%11 == 0
 }
