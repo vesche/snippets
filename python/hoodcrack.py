8f898a11ff4ef05e1a0db363f8aaa04a6f3a6717
Austin Jackson <austinjackson892@gmail.com>
2015-10-16 14:25:14 +0000

    fixed whitespace

--- python/hoodcrack.py
+++ python/hoodcrack.py
@@ -1,34 +1,34 @@
 import os
 
 def command_data(command):
-	return os.popen(command).read().split()
+    return os.popen(command).read().split()
 
 def netstat():
-	netstat = command_data("netstat -ano")
+    netstat = command_data("netstat -ano")
 
-	return netstat
+    return netstat
 
 def tasklist():
-	data = command_data("tasklist")
-	del data[0:21]
+    data = command_data("tasklist")
+    del data[0:21]
 
-	name, pid, mem_usage = [], [], []
+    name, pid, mem_usage = [], [], []
 
-	for i in range(len(data)):
-		try:
-			a, b, _, _, c, _ = data[0:6]
-			del data[0:6]
-		except:
-			break
+    for i in range(len(data)):
+        try:
+            a, b, _, _, c, _ = data[0:6]
+            del data[0:6]
+        except:
+            break
 
-		name.append(a)
-		pid.append(b)
-		mem_usage.append(c)
+        name.append(a)
+        pid.append(b)
+        mem_usage.append(c)
 
-	return name, pid, mem_usage
+    return name, pid, mem_usage
 
 def main():
-	print tasklist()
+    print tasklist()
 
 if __name__ == "__main__":
-	main()
+    main()
