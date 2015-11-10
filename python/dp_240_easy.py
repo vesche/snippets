b7101e0d72ba8913ecd12e9431ef864d2e1f0d44
Austin Jackson <austinjackson892@gmail.com>
2015-11-10 13:45:16 +0000

    bored

---
+++ python/dp_240_easy.py
@@ -0,0 +1,18 @@
+import random
+
+scram_words = []
+with open('input.txt', 'r') as f:
+    words = [list(_) for _ in f.read().split()]
+
+for word in words:
+    if len(word) < 3:
+        scram_words.append(''.join(word))
+    else:
+        first, scram, last = word.pop(0), "", word.pop()
+        while len(word):
+            letter = random.choice(word)
+            scram += letter
+            word.remove(letter)
+        scram_words.append(first + scram + last)
+
+print ' '.join(scram_words)
