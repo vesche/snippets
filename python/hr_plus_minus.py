bd64f66bd0f1516256e5cefd90a87c9642f5275d
Austin Jackson <austinjackson892@gmail.com>
2015-11-12 15:28:02 +0000

    asciify.py

--- python/hr_plus_minus.py
+++ python/hr_plus_minus.py
@@ -1,20 +1,16 @@
-def theshit(x, numbs):
-	x = float(x)
-	numbs = numbs.split(' ')
+def plusminus(x, numbs):
+    x 				= float(x)
+    numbs 			= numbs.split(' ')
+    pos, neg, zer 	= 0, 0, 0
 
-	pos, neg, zer = 0, 0, 0
+    for n in numbs:
+    	if   n[0] == '-': 	neg += 1
+    	elif n[0] == '0': 	zer += 1
+    	else: 				pos += 1
 
-	for n in numbs:
-		if n[0] == '-':
-			neg += 1
-		elif n[0] == '0':
-			zer += 1
-		else:
-			pos += 1
+    print '%.3f' % (pos / x)
+    print '%.3f' % (neg / x)
+    print '%.3f' % (zer / x)
 
-	print '%.3f' % (pos / x)
-	print '%.3f' % (neg / x)
-	print '%.3f' % (zer / x)
-
-theshit('6', '-2 -1 0 1 2 3')
-theshit('12', '-2 -1 0 1 2 3 4 5 6 7 8 9 10')
+plusminus('6', '-2 -1 0 1 2 3')
+plusminus('12', '-2 -1 0 1 2 3 4 5 6 7 8 9 10')
