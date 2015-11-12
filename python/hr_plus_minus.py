1616641f5f33fe5f73ded57342b7b0d7a0e37059
Austin Jackson <austinjackson892@gmail.com>
2015-11-12 15:31:28 +0000

    fixed format

--- python/hr_plus_minus.py
+++ python/hr_plus_minus.py
@@ -1,12 +1,12 @@
 def plusminus(x, numbs):
-    x 				= float(x)
-    numbs 			= numbs.split(' ')
-    pos, neg, zer 	= 0, 0, 0
+    x               = float(x)
+    numbs           = numbs.split(' ')
+    pos, neg, zer   = 0, 0, 0
 
     for n in numbs:
-    	if   n[0] == '-': 	neg += 1
-    	elif n[0] == '0': 	zer += 1
-    	else: 				pos += 1
+    	if   n[0] == '-': neg += 1
+    	elif n[0] == '0': zer += 1
+    	else:             pos += 1
 
     print '%.3f' % (pos / x)
     print '%.3f' % (neg / x)
--- python/hr_time_conversion.py
+++ python/hr_time_conversion.py
@@ -4,9 +4,9 @@ s, t, h = x[0:2], x[2:4], int(h)
 
 if t == 'PM':
 	if h == 12: h = 12
-	else: 		h += 12
+	else:       h += 12
 else:
 	if h == 12: h = 0
-	if h < 10: 	h = '0' + str(h)
+	if h < 10:  h = '0' + str(h)
 
 print ':'.join([str(h), m, s])
