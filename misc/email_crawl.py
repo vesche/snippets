d8e01a7a8fd994ec2fb73e5755f2f0642e28f5e7
vesche <austinjackson892@gmail.com>
2016-01-02 03:47:21 -0500

    v0.3 - code cleanup

--- misc/email_crawl.py
+++ misc/email_crawl.py
@@ -6,32 +6,34 @@ from collections import deque
 import re
 
 def crawl(url):
-    new_urls = deque(['http://{}'.format(url)])
-    processed_urls = set()
-    emails = []
+    new_urls        = deque(['http://{}'.format(url)])
+    processed_urls  = set()
+    emails          = []
 
     while len(new_urls):
         url = new_urls.popleft()
         processed_urls.add(url)
 
-        parts = urlsplit(url)
-        base_url = "{0.scheme}://{0.netloc}".format(parts)
-        path = url[:url.rfind('/')+1] if '/' in parts.path else url
+        parts       = urlsplit(url)
+        base_url    = "{0.scheme}://{0.netloc}".format(parts)
+        path        = url[:url.rfind('/') + 1] if '/' in parts.path else url
 
-        print "Processing %s" % url
+        print "Processing {}".format(url)
         try:
             response = requests.get(url)
-        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+        except (requests.exceptions.MissingSchema, \
+        requests.exceptions.ConnectionError):
             continue
 
-        new_emails = re.findall(r"\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+", response.text, re.I)
+        new_emails = re.findall(r"\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+", \
+        response.text, re.I)
         for addr in new_emails:
             emails.append(addr)
 
         soup = BeautifulSoup(response.text)
 
-        for anchor in soup.find_all("a"):
-            link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+        for anchor in soup.find_all('a'):
+            link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
             if link.startswith('/'):
                 link = base_url + link
             elif not link.startswith('http'):
