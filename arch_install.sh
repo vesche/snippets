bd50d99f050b6db36c7a8179901bc0eae7999cc3
vesche <austinjackson892@gmail.com>
2016-05-11 19:48:19 -0400

    arch install guide

---
+++ arch_install.sh
@@ -0,0 +1,111 @@
+#
+# This isn't intended to be run, it's a loose guide of how to install arch
+# from a live boot. Enjoy, vesche.
+
+################
+# arch install #
+################
+
+# update the system clock
+timedatectl set-ntp true
+
+# identify hard drive
+lsblk
+
+# partion
+parted /dev/sdx
+    (parted) mklabel msdos
+    (parted) mkpart primary ext4 1MiB 100%
+    (parted) set 1 boot on
+    (parted) quit
+
+# format fs & mount
+lsblk /dev/sdx
+mkfs.ext4 /dev/sdxN
+mount /dev/sdxN /mnt
+
+# install base
+pacstrap -i /mnt base base-devel
+
+# generate fstab
+genfstab -U /mnt > /mnt/etc/fstab
+
+# chroot
+arch-chroot /mnt /bin/bash
+
+# locale
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+# timezone
+ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc --utc
+
+# ramdisk
+mkinitcpio -p linux
+
+# bootloader
+pacman -S grub os-prober
+grub-install --recheck /dev/sdx
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# hostname
+echo 'hostname' > /etc/hostname
+
+# set root password
+passwd
+
+# reboot
+exit
+umount -R /mnt
+reboot
+
+################
+# post-install #
+################
+
+# create user
+useradd -m -G wheel -s /bin/bash user
+passwd user
+
+# uncomment wheel group
+visudo
+
+# leave root
+exit
+
+# dhcp
+sudo systemctl enable dhcpcd@eno1.service
+sudo systemctl start dhcpcd@eno1.service
+
+# package spam
+sudo pacman -S \
+cmus feh gcc git nano net-tools nitrogen nmap htop openssh profont ranger rtorrent \
+screenfetch scrot tcpreplay tree tmux unzip vim weechat wget yajl youtube-dl \
+python python-pip python-setuptools python2 python2-pip python2-setuptools \
+dmenu firefox vlc wireshark-cli wireshark-qt \
+xf86-video-intel xorg-server xorg-xinit xorg-xrandr bspwm sxhkd dmenu rxvt-unicode \
+
+# yaourt
+mkdir -p ~/tmp/AUR && cd $_
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+tar xfz package-query.tar.gz  # unpack tarball
+cd package-query  &&  makepkg  # cd and create package from source
+sudo pacman -U package-query*.pkg.tar.xz  
+cd ~/tmp/AUR
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+tar xzf yaourt.tar.gz
+cd yaourt && makepkg
+sudo pacman -U yaourt*.pkg.tar.xz
+
+# AUR
+yaourt -S atom-editor lemonbar-git spotify
+
+# dotfiles
+git clone https://github.com/vesche/dotfiles
+cp dotfiles/{.Xresources, .bar.py, .bash_profile, .bashrc, .vimrc, .xinitrc} ~/
+cp -R dotfiles/.config ~/
+
+# firefox add-ons: imagus, ublock, stylish
+# stylish themes: github, hacker news, reddit, stackoverflow, wikipedia
+
