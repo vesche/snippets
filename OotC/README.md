4f76a7046c61fe84c5d97b3277816dba5ab631bf
vesche <austinjackson892@gmail.com>
2016-06-04 19:52:46 -0400

    OotC initial commit

---
+++ OotC/README.md
@@ -0,0 +1,1 @@
+Order of the Characters (OotC), unfinished concept multi-user dungeon (MUD) I was hacking together because I was bored.
---
+++ OotC/common.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+
+# the ascii logo was generated on http://patorjk.com/software/taag
+menu_text = '''
+  .g8""8q.             mm     .g8"""bgd
+.dP'    `YM.           MM   .dP'     `M
+dM'      `MM  ,pW"Wq.mmMMmm dM'       `
+MM        MM 6W'   `Wb MM   MM
+MM.      ,MP 8M     M8 MM   MM.
+`Mb.    ,dP' YA.   ,A9 MM   `Mb.     ,'
+  `"bmmd"'    `Ybmd9'  `Mbmo  `"bmmmd'
+
+1. Play
+2. About
+3. Quit
+'''
+
+about_text = 'Order of the Characters (OotC) is a minimalistic Multi-User \
+Dungeon (MUD). Each player chooses a special character to be represented as, \
+which defines how the game is played. Alliances will be formed, wars will be \
+waged- is your character superior?'
