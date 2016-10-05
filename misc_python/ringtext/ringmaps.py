94b2570e13327456cea896648fb4335d11c46aee
vesche <austinjackson892@gmail.com>
2016-10-04 22:53:00 -0400

    drunk textgames

---
+++ misc_python/ringtext/ringmaps.py
@@ -0,0 +1,42 @@
+# +---------------+
+# |      ...      |
+# |      .G.      |
+# |   .........   |
+# |   .F.   .C.   |
+# |......   ......|
+# |WF.    .    EF.|
+# |......   ......|
+# |   .L.   .P.   |
+# |   .........   |
+# |      .D.      |
+# |      ...      |
+# +---------------+
+
+def ringtext(x, y):
+    text = ' '
+    message =   "|{0: <15}|\n" \
+                "+---------------+"
+
+    if (7 <= x <= 9) and (3 <= y <= 5):
+        text += "Garden"
+
+    elif (4 <= x <= 6) and (5 <= y <= 7):
+        text += "Forest"
+
+    elif (10 <= x <= 12) and (5 <= y <= 7):
+        text += "City"
+
+    elif (1 <= x <= 3) and (7 <= y <= 9):
+        text += "West Fountain"
+
+    elif (13 <= x <= 15) and (7 <= y <= 9):
+        text += "East Fountain"
+
+    #if (x, y) == (7, 3):
+    #    return "garden 0"
+    #elif (x, y) == (8, 3):
+    #    return "garden 1"
+    #elif (x, y) == (9, 3):
+    #    return "garden 2"
+
+    return message.format(text)
---
+++ misc_python/ringtext/ringtext.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+
+import readline
+from os import system
+from ringmaps import ringtext
+
+playing = True
+
+ring =  "+---------------+\n" \
+        "| RingText v0.1 |\n" \
+        "+---------------+\n" \
+        "|      ...      |\n" \
+        "|      ...      |\n" \
+        "|   .........   |\n" \
+        "|   ...   ...   |\n" \
+        "|......   ......|\n" \
+        "|...    .    ...|\n" \
+        "|......   ......|\n" \
+        "|   ...   ...   |\n" \
+        "|   .........   |\n" \
+        "|      ...      |\n" \
+        "|      ...      |\n" \
+        "+---------------+"   \
+        "---------------+"
+
+ring = [[tile for tile in line] for line in ring.split('\n')]
+
+def print_map(x, y):
+    ring[y][x] = '@'
+    print '\n'.join([''.join(line) for line in ring])
+    return
+
+def main():
+    x, y = 8, 4
+
+    while playing:
+        system("clear")
+
+        print_map(x, y)
+
+        print ringtext(x, y)
+        move = raw_input("> ").lower()
+
+        ring[y][x] = '.'
+
+        if move == 'n' and ring[y-1][x] == '.':
+            y -= 1
+        elif move == 's' and ring[y+1][x] == '.':
+            y += 1
+        elif move == 'e' and ring[y][x+1] == '.':
+            x += 1
+        elif move == 'w' and ring[y][x-1] == '.':
+            x -= 1
+
+if __name__ == "__main__":
+    main()
+
