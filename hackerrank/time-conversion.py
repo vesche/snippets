2eec70b5b35c08e77a505fa95a76702b3ce748cf
vesche <austinjackson892@gmail.com>
2016-10-17 21:24:37 -0400

    Fix some style issues

--- hackerrank/time-conversion.py
+++ hackerrank/time-conversion.py
@@ -9,16 +9,20 @@
 
 
 def main():
-	line 	= raw_input()
+	line = raw_input()
 	h, m, x = line.split(':')
 	s, t, h = x[0:2], x[2:4], int(h)
 
 	if t == "PM":
-		if h == 12: h = 12
-		else:       h += 12
+		if h == 12:
+            h = 12
+		else:
+            h += 12
 	else:
-		if h == 12: h = 0
-		if h < 10:  h = '0' + str(h)
+		if h == 12:
+            h = 0
+		if h < 10:
+            h = '0' + str(h)
 
 	print ':'.join([str(h), m, s])
 
