85b2e3a1a96837884ae6d3b3f09a886acc82600c
vesche <austinjackson892@gmail.com>
2016-11-01 21:17:15 -0400

    fixed 290 easy

--- dailyprogrammer/290_easy.py
+++ dailyprogrammer/290_easy.py
@@ -10,14 +10,10 @@ lower, upper = map(int, raw_input().split())
 
 for n in range(lower, upper+1):
     ns = str(n**2)
-    for i in range(len(ns)):
-        try:
-            a, b = int(ns[:i]), int(ns[i:])
-        except ValueError:
-            continue
 
-        if a == 0 or b == 0:
-            continue
-        if a + b == n:
+    for i in range(1, len(ns)):
+        a, b = int(ns[:i]), int(ns[i:])
+
+        if a + b == n and a != 0 and b != 0:
             print n,
             continue
