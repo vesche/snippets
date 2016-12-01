d17fe15d830c4d1806c5a9c02593186d4b116809
Austin Jackson <austinjackson892@gmail.com>
2016-12-01 20:36:29 +0000

    finish aoc 2016 day 1

--- adventofcode_2016/day01_part2.py
+++ adventofcode_2016/day01_part2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 ######################################
-# Advent of Code 2016, Day 01 - Part 1
+# Advent of Code 2016, Day 01 - Part 2
 # https://github.com/vesche
 ######################################
 
@@ -10,7 +10,8 @@ def main():
     #    N, E, S, W
     d = [0, 0, 0, 0]
     facing = 0
-
+    locations = []
+    
     with open("day01_input.txt") as f:
         data = f.read().strip().split(', ')
     
@@ -24,10 +25,23 @@ def main():
             facing -= 1
         
         facing %= 4
-        d[facing] += blocks
-    
-    print abs(d[0] - d[2]) + abs(d[1] - d[3])
+        
+        for i in range(1, blocks+1):
+            if facing == 0:
+                d[0] += 1
+            elif facing == 1:
+                d[1] += 1
+            elif facing == 2:
+                d[2] += 1
+            elif facing == 3:
+                d[3] += 1
+            
+            x, y = d[1] - d[3], d[0] - d[2]
+            locations.append([x, y])
+            
+            if locations.count([x, y]) == 2:
+                return abs(x) + abs(y)
 
 
 if __name__ == "__main__":
-    main()
+    print main()
