4d5dd93f1f7e85ab10a714c52041b686dbed6d61
vesche <austinjackson892@gmail.com>
2016-12-03 13:00:25 -0500

    modify input

--- misc_python/asciify.py
+++ misc_python/asciify.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-################################
+#####################################################################
 # asciify.py
 # Turns an image into ASCII art.
 # Essentially a rewrite of - https://gist.github.com/cdiener/10491632
 # https://github.com/vesche
-################################
+#####################################################################
 
+import sys
 from PIL import Image
 import numpy as np
 
@@ -24,7 +25,7 @@ def asciify(f):
 
 
 def main():
-    image_file = "whatever.jpg"
+    image_file = sys.argv[1]
     print(asciify(image_file))
 
 
