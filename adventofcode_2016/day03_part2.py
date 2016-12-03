4bc0308c02d4bb3325119821c8644b20fa2f9cea
vesche <austinjackson892@gmail.com>
2016-12-03 13:59:45 -0500

    fix header aoc day 3

--- adventofcode_2016/day03_part2.py
+++ adventofcode_2016/day03_part2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 ######################################
-# Advent of Code 2016, Day 03 - Part 1
+# Advent of Code 2016, Day 03 - Part 2
 # https://github.com/vesche
 ######################################
 
