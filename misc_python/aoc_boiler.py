34760ef22e7d416f4850e8e49e9f6a79e214a8b3
vesche <austinjackson892@gmail.com>
2016-12-03 12:56:57 -0500

    aoc boilerplate

---
+++ misc_python/aoc_boiler.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+######################################
+# Advent of Code 2016, Day 00 - Part 0
+# https://github.com/vesche
+######################################
+
+def main():
+    with open("day00_input.txt") as f:
+        data = f.read()
+
+
+if __name__ == "__main__":
+    main()
