ee77a0d5a872816fd496c5721d13ca017237c31a
vesche <austinjackson892@gmail.com>
2016-12-04 12:20:04 -0500

    aoc day 4

---
+++ adventofcode_2016/day04_part1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+######################################
+# Advent of Code 2016, Day 04 - Part 1
+# https://github.com/vesche
+######################################
+
+from collections import Counter
+
+
+def main():
+    total = 0
+
+    with open("day04_input.txt") as f:
+        data = f.read().splitlines()
+
+    for line in data:
+        line = line.split('-')
+
+        name = ''.join(line[:-1])
+        sector = int(line[-1][:3])
+        checksum = line[-1][4:9]
+
+        top5 = ''.join([i for i, _ in sorted(Counter(name).most_common(10),
+        key=lambda tup: (-tup[1], tup[0]))])[:5]
+
+        if checksum == top5:
+            total += sector
+
+    return total
+
+
+if __name__ == "__main__":
+    print main()
---
+++ adventofcode_2016/day04_part2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+######################################
+# Advent of Code 2016, Day 04 - Part 2
+# https://github.com/vesche
+######################################
+
+from collections import Counter
+from string import ascii_lowercase
+
+
+def decrypt_rotn(enc_data, n):
+    dec = ''
+    n %= 26
+    for i in enc_data.lower():
+        try:
+            offset = n + ascii_lowercase.index(i)
+            if offset >= 25:
+                offset -= 26
+            char = ascii_lowercase[offset]
+        except ValueError:
+            char = i
+        dec += char
+    return dec
+
+
+def main():
+    with open("day04_input.txt") as f:
+        data = f.read().splitlines()
+
+    for line in data:
+        line = line.split('-')
+
+        name = ''.join(line[:-1])
+        sector = int(line[-1][:3])
+        checksum = line[-1][4:9]
+
+        top5 = ''.join([i for i, _ in sorted(Counter(name).most_common(10),
+        key=lambda tup: (-tup[1], tup[0]))])[:5]
+
+        if checksum == top5:
+            print sector, decrypt_rotn(' '.join(line[:-1]), sector)
+
+
+if __name__ == "__main__":
+    main()
