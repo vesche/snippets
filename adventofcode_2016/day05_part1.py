2271f52c9e4ddb4914fb725c83a591fe299f7864
Austin Jackson <austinjackson892@gmail.com>
2016-12-06 21:21:53 -0500

    aoc day 5

---
+++ adventofcode_2016/day05_part1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+######################################
+# Advent of Code 2016, Day 05 - Part 1
+# https://github.com/vesche
+######################################
+
+import itertools
+import hashlib
+
+
+def md5hash(string):
+    m = hashlib.md5()
+    m.update(string)
+    return m.hexdigest()
+
+
+def main():
+    door_id = "ojvtpuvg"
+    password = ''
+
+    for n in itertools.count():
+
+        h = md5hash(door_id + str(n))
+
+        if h[:5] == "00000":
+            password += h[5]
+
+            if len(password) == 8:
+                break
+
+    return password
+
+
+if __name__ == "__main__":
+    print main()
---
+++ adventofcode_2016/day05_part2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+######################################
+# Advent of Code 2016, Day 05 - Part 2
+# https://github.com/vesche
+######################################
+
+import itertools
+import hashlib
+
+
+def md5hash(string):
+    m = hashlib.md5()
+    m.update(string)
+    return m.hexdigest()
+
+
+def show(password):
+    print "\033c" + ''.join(password)
+
+
+def main():
+    door_id = "ojvtpuvg"
+    password = list("________")
+    show(password)
+
+    for n in itertools.count():
+
+        h = md5hash(door_id + str(n))
+
+        if h[:5] == "00000":
+            try:
+                if password[int(h[5])] == '_':
+                    password[int(h[5])] = h[6]
+            except:
+                pass
+
+            show(password)
+
+            if '_' not in password:
+                break
+
+
+if __name__ == "__main__":
+    main()
