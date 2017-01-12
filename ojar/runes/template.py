86c383adb4cece14e24440c5d8247092b59084f3
vesche <austinjackson892@gmail.com>
2017-01-12 17:12:57 -0500

    migrate ojar out of snippets

--- ojar/runes/template.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# [?] rune
-
-
-def info(_):
-    from common import list_functions
-    return "[?] rune\nactions: {}".format(list_functions(__name__))
