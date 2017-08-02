b3a3a8316adca7316d53c973f9bd2822786145f4
Austin Jackson <austinjackson892@gmail.com>
2017-08-02 16:46:29 +0000

    Oh, rust? Why do you hate me so.

--- misc_rust/textgame.rs
+++ misc_rust/textgame.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::Write;
+use std::thread;
 
 const BANNER: &'static str = "
 '||''|.   |''||''|     |    
@@ -25,83 +26,101 @@ fn main() {
     ["|","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_", "_","_","_","|"]];
     
     // starting position
-    let mut pos_y = 5;
-    let mut pos_x = 10;
+    let mut pos_y: i32 = 5;
+    let mut pos_x: i32 = 10;
+    
+    // initial variables
+    let mut mod_y: i32 = 0;
+    let mut mod_x: i32 = 0;
     let mut level = 0;
+    let mut message = "You awake in a dark, damp jungle...";
 
     // start game
     clear_screen();
     println!("{}\n", BANNER);
     let name = input("What's your name? ");
-    println!("Hello there, {}!", name);
     
     // main game loop
     loop {
-        // print map
+        // clear the screen
         clear_screen();
-        map[pos_y][pos_x] = "@";
-        println!("RTA | {} | LV: {}", name, level);
+        
+        // set player position
+        map[pos_y as usize][pos_x as usize] = "@";
+        
+        // print map / game
+        println!("RTA | {} | LV: {} | y: {} x: {}", name, level, pos_y, pos_x);
         for i in 0..11 {
             println!("{}", map[i].join(""));
         }
+        xprint(message);
+        // println!("{}", map_desc(pos_y, pos_x));
         
-        // reset map
-        map[pos_y][pos_x] = "*";
+        // reset player position and message
+        map[pos_y as usize][pos_x as usize] = "*";
+        message = "";
+        mod_y = 0;
+        mod_x = 0;
         
         // get player move
         let mut player_move = String::new();
         player_move = input("> ");
+        player_move = player_move.to_lowercase();
         
-        // player move control flow
-        if player_move == "north" {
-            if map[pos_y-1][pos_x] == " " {
-                continue;
-            }
-            pos_y -= 1;
+        // act on player move
+        if player_move == "north" || player_move == "n" {
+            mod_y = -1;
         }
-        else if player_move == "south" {
-            if map[pos_y+1][pos_x] == " " {
-                continue;
-            }
-            pos_y += 1;
+        else if player_move == "south" || player_move == "s" {
+            mod_y = 1;
         }
-        else if player_move == "east" {
-            if map[pos_y][pos_x+1] == " " {
-                continue;
-            }
-            pos_x += 1;
+        else if player_move == "east" || player_move == "e" {
+            mod_x = 1;
         }
-        else if player_move == "west" {
-            if map[pos_y][pos_x-1] == " " {
-                continue;
-            }
-            pos_x -= 1;
+        else if player_move == "west" || player_move == "w" {
+            mod_x = -1;
         }
-        else if player_move == "quit" {
+        else if player_move == "quit" || player_move == "q" {
             break;
         }
         else {
             println!("Invalid move!");
         }
+        
+        // check
+        if map[(pos_y + mod_y) as usize][(pos_x + mod_x) as usize] == " " {
+            message = "You can't go that way!";
+        } else {
+            pos_y += mod_y;
+            pos_x += mod_x;
+        }
+        
     }
 }
 
 /*
-fn map_desc(pos_y: i32, pos_x: i32) -> String {
+fn map_desc(pos_y: usize, pos_x: usize) -> String {
+    // Returns location description given a (y, x) coordinate
 
-    let coords = (pos_y, pos_x);
-    let test = [[ (4, 10), ("Beach", 0) ], [ (5, 10), ("Jungle", 0) ]];
+    // let desc = String::new();
+    let desc = "";
+    let coords = [(4, 10), (5, 10)];
+    let test = ["Jungle", "Beach"];
     
-    for i in 0..2 {
-        if test[i][0] == coords {
-            "Test"
+    for i in 0..coords.len() {
+        if (pos_y, pos_x) == coords[i] {
+            let desc = test[i];
         }
     }
-
+    
+    desc.to_string()
 }
 */
 
 fn input(prompt: &str) -> String {
+    // Returns a user input string without a trailing new line.
+    // Accepts a prompt to display to the user before input.
+
     // write prompt to screen
     print!("{}", prompt);
     io::stdout().flush().unwrap();
@@ -114,6 +133,20 @@ fn input(prompt: &str) -> String {
     // strip new line and return
     user_input.pop();
     user_input
+}
+
+fn xprint(words: &str) {
+    // Given a string, prints each character with .05 second increments
+
+    for i in 0..words.len() {
+        // print single letter
+        print!("{}", words.chars().nth(i).unwrap());
+        io::stdout().flush().unwrap();
+
+        // sleep for .05 seconds
+        thread::sleep_ms(50);
+    }
+    println!();
 }
 
 fn clear_screen() {
