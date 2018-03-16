ecf9fa3aed7729c96a81d7a2d29d4dcda0721624
vesche <austinjackson892@gmail.com>
2018-03-16 15:29:09 -0400

    clean up snippets

--- nim/tutorial_01.nim
+++
@@ -1,6 +0,0 @@
-
-# this is a comment
-
-echo "What's your name? "
-var name: string = readLine(stdin)
-echo "Hi, ", name, "!"
--- python/poopdb/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p /opt/poopdb
-yes | cp -rf ./poopdb/* /opt/poopdb
